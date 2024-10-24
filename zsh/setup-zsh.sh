be38636d3e6c9b379665e0f82142885cb09e1fcf
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2024-10-24 18:09:34 +0200

    better setup zsh script

--- zsh/setup-zsh.sh
+++ zsh/setup-zsh.sh
@@ -4,4 +4,6 @@ mkdir -p $HOME/.zsh-plugins/
 
 git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/.zsh-plugins/.zsh-vi-mode
 
+cp ./zshrc $HOME/.zshrc
+
 echo "please install the following packages before changing shell: zsh-syntax-highlighting, zsh-autosuggestions"
