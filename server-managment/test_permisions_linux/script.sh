96f0f95e7ec259dc5e8a2718fd4c96ec8c4b5aa0
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2024-11-28 11:43:37 +0100

    debug color in test

--- server-managment/test_permisions_linux/script.sh
+++ server-managment/test_permisions_linux/script.sh
@@ -1,5 +1,11 @@
+
 #!/bin/bash
-archivo="test.txt"
+archivo="newusers.txt"
+
+# Colores
+COLOR_SUCCESS='\033[0;32m'  # Verde para éxito
+COLOR_FAIL='\033[0;31m'     # Rojo para fallo
+COLOR_RESET='\033[0m'       # Restablecer el color al valor predeterminado
 
 # user(0) mode to try(1) path(2) expected result(3)
 while IFS= read -r line; do
@@ -24,32 +30,31 @@ while IFS= read -r line; do
         # Probar si el usuario tiene permisos de lectura
         if sudo -u "$usuario" test -r "$ruta"; then
             if [[ "$esperado" == "1" ]]; then
-                echo "success in $usuario $modo $ruta"
+                echo -e "${COLOR_SUCCESS}success in $usuario $modo $ruta${COLOR_RESET}"
             else
-                echo "fail in $usuario $modo $ruta"
+                echo -e "${COLOR_FAIL}fail in $usuario $modo $ruta${COLOR_RESET}"
             fi
         else
             if [[ "$esperado" == "1" ]]; then
-                echo "fail in $usuario $modo $ruta"
+                echo -e "${COLOR_FAIL}fail in $usuario $modo $ruta${COLOR_RESET}"
             else
-                echo "success in $usuario $modo $ruta"
+                echo -e "${COLOR_SUCCESS}success in $usuario $modo $ruta${COLOR_RESET}"
             fi
         fi
     elif [[ "$modo" == "w" ]]; then
         # Probar si el usuario tiene permisos de escritura
         if sudo -u "$usuario" test -w "$ruta"; then
             if [[ "$esperado" == "1" ]]; then
-                echo "success in $usuario $modo $ruta"
+                echo -e "${COLOR_SUCCESS}success in $usuario $modo $ruta${COLOR_RESET}"
             else
-                echo "fail in $usuario $modo $ruta"
+                echo -e "${COLOR_FAIL}fail in $usuario $modo $ruta${COLOR_RESET}"
             fi
         else
             if [[ "$esperado" == "1" ]]; then
-                echo "fail in $usuario $modo $ruta"
+                echo -e "${COLOR_FAIL}fail in $usuario $modo $ruta${COLOR_RESET}"
             else
-                echo "success in $usuario $modo $ruta"
+                echo -e "${COLOR_SUCCESS}success in $usuario $modo $ruta${COLOR_RESET}"
             fi
         fi
     fi
 done < "$archivo"
-
