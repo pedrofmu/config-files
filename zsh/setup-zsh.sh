d2fffa635eaebef826197e657993ba5094f2d679
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2024-12-23 16:05:31 +0100

    change zsh ls

--- zsh/setup-zsh.sh
+++ zsh/setup-zsh.sh
@@ -7,3 +7,4 @@ git clone https://github.com/jeffreytse/zsh-vi-mode.git $HOME/.zsh-plugins/zsh-v
 cp ./zshrc $HOME/.zshrc
 
 echo "please install the following packages before changing shell: zsh-syntax-highlighting, zsh-autosuggestions"
+echo "it is also recomendable to install: 'exa, deno, distro-box, atuin and bat'\n if you dont want to use them make sure to change .zshrc acordingly"
