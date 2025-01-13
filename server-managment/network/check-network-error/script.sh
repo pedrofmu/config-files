ea96e5943f19b032dc154c94c364629b462c7470
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2025-01-13 09:46:31 +0100

    finished the network script, starting testing

--- server-managment/network/check-network-error/script.sh
+++ server-managment/network/check-network-error/script.sh
@@ -1,6 +1,44 @@
-#!/bin/dash
+#!/bin/sh
 
-GATEWAY=$(ip route | grep "default" | head -n 1 | cut -d " " -f 3)
+check_dependencies() {
+    for cmd in ip ping bc grep curl; do
+        if ! command -v $cmd > /dev/null 2>&1; then
+            echo "Command $cmd is required but not installed. Some parts of the script might fail."
+            echo "What do you want to do [ continue (c) / exit (e) ]"
+            read result
+            if [ "$result" = "e" ]; then
+                exit
+            fi
+        fi
+    done
+}
+check_dependencies
+
+GATEWAY=$(ip route | awk '/default/ {print $3; exit}')
+
+to_dotted_dec() {
+    local num="$1"
+    printf "%d.%d.%d.%d" \
+        $(( (num >> 24) & 0xFF )) \
+        $(( (num >> 16) & 0xFF )) \
+        $(( (num >>  8) & 0xFF )) \
+        $((  num        & 0xFF ))
+}
+
+display_help() {
+    case "$1" in
+        "gateway_missing") echo "the route is not configured, you can try configuring a static ip or setting up a DHCP server." 
+        ;;
+        "gateway_unreacheable") echo "gateway is unreachable, it might be a problem with the firewall or a gateway not present in your network" 
+        ;;
+        "ping_internet_failed") echo "ping internet failed, it might be a bad configuration in the routing" 
+        ;;
+        "dns_resolve_failed") echo "the resolve DNS didnt work, check the dns client configuration and the DNS server configuration" 
+        ;;
+        *) echo default
+        ;;
+    esac
+}
 
 try_dhcp() {
     posible_dhcp_clients="dhclient dhcpd" 
@@ -9,19 +47,22 @@ try_dhcp() {
     do
         command -v $dhcp_client > /dev/null 2>&1 
         if [ $? -eq 0 ]; then
+            echo "use the root password"
             case "$dhcp_client" in
                 "dhclient")
-                    echo "use the root password"
                     su -c "dhclient -r && dhclient"
                     sleep 10 
-                    GATEWAY=$(ip route | grep "default" | head -n 1 | cut -d " " -f 3)
                 ;;
                 *) echo "error not implemented dhcp client" 
                 ;;
             esac
-             
         fi
     done
+    GATEWAY=$(ip route | awk '/default/ {print $3; exit}')
+    if [ -z "$GATEWAY" ]; then
+        echo "Failed to obtain a gateway via DHCP."
+        return 1
+    fi
 }
 
 scan_local_network() {
@@ -34,17 +75,46 @@ scan_local_network() {
 
     set -- $(echo $ip | sed 's/\./ /g')
 
-    echo "$1 $2 $3 $4 | $total_ips"
-}
+    # Convertir IP a número de 32 bits
+    ip_num=$(( ($1 << 24) + ($2 << 16) + ($3 << 8) + $4 ))
 
-scan_local_network
+    # Crear la máscara de red a partir del CIDR
+    # Ejemplo: /24 -> 255.255.255.0 en binario es 11111111.11111111.11111111.00000000
+    # Máscara 32 bits: 0xFFFFFFFF << (32 - cidr)
+    mask_num=$(( 0xFFFFFFFF << (32 - $prefix) & 0xFFFFFFFF ))
+
+    network_num=$(( ip_num & mask_num ))
+
+    host_count=0
+    if [ "$prefix" -lt 31 ]; then
+        host_count=$(( (1 << (32 - $prefix)) - 2 ))
+    elif [ "$prefix" -eq 31 ]; then
+        # /31: Usualmente se usan en enlaces punto a punto, hay 2 direcciones sin host reservados
+        host_count=2
+    elif [ "$prefix" -eq 32 ]; then
+        # /32: Solo una dirección host
+        host_count=1
+    fi
+
+    i=1
+    while [ "$i" -le "$host_count" ]; do
+        host_ip=$(to_dotted_dec "$((network_num + i))")
+        ping -c 1 -W 2 "$host_ip" > /dev/null 2>&1 
+        if [ "$?" -eq 0 ]; then
+            echo "$host_ip is up, check if it is a valid gateway"
+        fi
+        i=$((i+1))
+    done
+}
 
 if [ -z "$GATEWAY" ]; then
     echo "The gateway is missing, check your IP configuration with 'ip a'."
-    echo "What do you want to try? [search for DHCP (s) / exit (e)]"
+    echo "What do you want to try? [search for DHCP (s) / display common help (h) / exit (e)]"
     read result
     if [ "$result" = "s" ]; then
         try_dhcp
+    elif [ "$result" = "h" ]; then
+         display_help "gateway_missing"  
     else
         exit
     fi
@@ -54,11 +124,13 @@ fi
 
 ping -c 1 "$GATEWAY" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "Gateway $GATEWAY is unreachable."
-    echo "What do you want to try? [scan for devices in network (s) / exit (e)]"
+    echo "Gateway $GATEWAY is unreacheable."
+    echo "What do you want to try? [scan for devices in network (s) / display common help (h) / exit (e)]"
     read result
     if [ "$result" = "s" ]; then
         scan_local_network
+    elif [ "$result" = "h" ]; then
+         display_help "gateway_unreacheable"  
     else
         exit
     fi
@@ -75,13 +147,19 @@ if [ $? -ne 0 ]; then
         if [ $? -eq 0 ]; then
             echo "Internet is reachable; it was a problem with ping."
         else
-            echo "Internet is unreachable. Try contacting your network administrator for more information."
-            exit
+            echo "Internet is unreachable. What do you want to do? [display common help (h) / exit (e)]"
+            read result
+            if [ "$result" = "h" ]; then
+                display_help "ping_internet_failed"
+            else
+                exit
+            fi
         fi
     fi
 else
     echo "The internet is reachable."
 fi
+
 
 ping -c 1 google.com > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -92,8 +170,13 @@ if [ $? -ne 0 ]; then
         if [ $? -eq 0 ]; then
             echo "Internet with DNS worked; it was a problem with ping."
         else
-            echo "Internet is unreachable. Try contacting your network administrator for more information."
-            exit
+            echo "Dns didnt work. What do you want to do? [display common help (h) / exit (e)]"
+            read result
+            if [ "$result" = "h" ]; then
+                display_help "dns_resolve_failed"
+            else
+                exit
+            fi
         fi
     else
         exit
