618d90088ef0080bd70a1f3548e9f61ad7e5c75f
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2025-02-12 09:42:44 +0100

    better create bash users scripts

--- server-managment/create-users-bash/script.sh
+++ server-managment/create-users-bash/script.sh
@@ -8,10 +8,12 @@ while IFS= read -r linea; do
     for grp in "${group_array[@]}"; do
        getent group "$grp" || groupadd "$grp" 
     done
-  useradd -s "${array[4]}" -G "${array[2]}" -p "${array[1]}" "${array[0]}"
+  useradd -s "${array[4]}" -G "${array[2]}" "${array[0]}"
+  echo "${array[0]}:${array[1]}" | chpasswd
 
   IFS=',' read -r -a home_array <<< "${array[3]}"
   if [ "${home_array[0]}" == "1" ]; then
         mkdir -p "${home_array[1]}"
+        chmod -R "${array[0]}" "${home_array[1]}"
   fi
 done < "$archivo" 
